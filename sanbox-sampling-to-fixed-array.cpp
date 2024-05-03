dafc7c912144a02a3a930e666a9e9e8f87a5933b
Ukhem Fahmi <104829519+UFTHaq@users.noreply.github.com>
2024-05-03 22:22:36 +0800

    Update sanbox-sampling-to-fixed-array.cpp
    Add index interval Formula

--- sanbox-sampling-to-fixed-array.cpp
+++ sanbox-sampling-to-fixed-array.cpp
@@ -5,7 +5,7 @@ int main() {
     // Write C++ code here
     std::cout << "Try programiz.pro\n\n";
     
-    int size = 22129;
+    int size = 26227;
     std::cout << "size : " << size << std::endl;
     int N{1000};
     
@@ -40,23 +40,69 @@ int main() {
     // float b = x2 + y;
     // std::cout << "b : " << b << std::endl << std::endl;
     
-    int coef_a_x = N * a * interval_a;
-    std::cout << "coef_a_x -> " << interval_a << " : " << coef_a_x << std::endl;
-    int coef_b_x = N * b * interval_b;
-    std::cout << "coef_b_x -> " << interval_b << " : " << coef_b_x << std::endl;
+    float coef_a_x = N * a * interval_a;
+    float coef_b_x = N * b * interval_b;
+    
+    std::cout << "coef_a_x -> " << interval_a << " x " << (float)coef_a_x / interval_a << " = " << coef_a_x << std::endl;
+    std::cout << "coef_b_x -> " << interval_b << " x " << (float)coef_b_x / interval_b << " = " << coef_b_x << std::endl;
     
     int used = coef_a_x + coef_b_x;
     std::cout << "used : " << used << std::endl;
     
     int remain = size - used;
-    std::cout << "remain : " << remain << std::endl << std::endl;
+    std::cout << "remain : " << remain << std::endl;
+    std::cout << std::endl;
     
     float new_coef{};
     if (coef_a_x > coef_b_x) new_coef = (float)coef_a_x / coef_b_x;
     else new_coef = (float)coef_b_x / coef_a_x;
     
     std::cout << "new coef : " << new_coef << std::endl;
+    std::cout << std::endl;
+    
+    float pengali_a = ((float)coef_a_x / interval_a);
+    float pengali_b = ((float)coef_b_x / interval_b);
+    
+    float new_coef2{};
+    int high_intense_interval{};
+    int low_intense_interval{};
+    if (coef_a_x > coef_b_x) {
+        new_coef2 = pengali_a / pengali_b;
+        high_intense_interval = interval_a;
+        low_intense_interval = interval_b;
+    }
+    else {
+        new_coef2 = pengali_b / pengali_a;
+        high_intense_interval = interval_b;
+        low_intense_interval = interval_a;
+    }
+
+    float lower_pengali{};
+    float higher_pengali{};
+    if (pengali_a > pengali_b) {
+        higher_pengali = pengali_a;
+        lower_pengali = pengali_b;
+    } else {
+        lower_pengali = pengali_a;
+        higher_pengali = pengali_b;
+    }
+    std::cout << "new coef2 : " << new_coef2 << std::endl;
+    int seling = int(new_coef2);
+    std::cout << "selang-seling tiap : " << seling << "x" << std::endl;
+    
+    int hasil_lower_and_new_coef2 = lower_pengali * seling;
+    std::cout << "taktik : " << lower_pengali << " x " << seling << " = " << hasil_lower_and_new_coef2 << std::endl;
+    int sisa = higher_pengali - hasil_lower_and_new_coef2;
+    std::cout << "hasil : " << higher_pengali << " - " << hasil_lower_and_new_coef2 << " = " << sisa << std::endl;
+    std::cout << "sisa " << sisa << " kali untuk " << high_intense_interval << std::endl;
+    
+    std::cout << std::endl;
+    
+    std::cout << "Formula Interval Sampling: \n";
+    std::cout << "Step index : " << low_intense_interval << " x 1 -> " << high_intense_interval << " x " << seling << std::endl;
+    
     
     
     return 0;
+    
 }
